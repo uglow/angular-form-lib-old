f84bc8838aab9a2aec3102a544adf673242de070
Brett Uglow <buglow@odecee.com.au>
2014-12-28 16:40:24 +1100

    chore(build): Update build process to include documentation commits when releasing.

---
+++ config/grunt/releaseDocs.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var colors = require('colors'); /* jshint ignore:line */
+
+  var config = grunt.config.get('paths.release');
+
+  grunt.extendConfig({
+
+    // Documentation tasks
+    gitcheckout: {
+      documentation: {
+        options: {
+          branch: 'gh-pages'
+        }
+      },
+      master: {
+        options: {
+          branch: 'master'
+        }
+      }
+    },
+
+    gitadd: {
+      documentation: {
+        options: {
+          all: true
+        },
+        files: {
+          src: ['site/**/*']
+        }
+      }
+    },
+
+    gitcommit: {
+      documentation: {
+        options: {
+          message: 'docs(v' + grunt.file.readJSON('package.json').version + '): Update documentation',
+          noStatus: true
+        },
+        files: {
+          src: ['site/**/*']
+        }
+      }
+    },
+
+    copy: {
+      docsToSite: {
+        files: [{expand: true, cwd: 'docs/', src: '**/*', dest: 'site/'}]
+      }
+    }
+  });
+
+  grunt.registerTask('releaseDocs', ['gitcheckout:documentation', 'copy:docsToSite', 'gitadd:documentation', 'gitcommit:documentation',
+                                     'gitcheckout:master']);
+
+};
